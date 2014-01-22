e62ef9da9f0ef872a334a0459025762a3bb1bc5b
William Budington <bill@eff.org>
2014-01-22 13:30:13 -0800

    adding as_required_json test to CongressMemberAction

--- spec/models/congress_member_action_spec.rb
+++ spec/models/congress_member_action_spec.rb
@@ -12,4 +12,10 @@ describe CongressMemberAction do
       create :congress_member_action, action: "testing"
     end.to raise_error(ActiveRecord::RecordInvalid)
   end
+
+  it "should only have the required fields when calling as_required_json" do
+    ca = create :congress_member_action, action: "fill_in", name: "first-name", value: "first-name"
+    expect(ca.as_json["id"]).not_to be_nil
+    expect(ca.as_required_json["id"]).to be_nil
+  end
 end
