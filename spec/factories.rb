5c674a82b8d584a5331b76a9f2d24bf2d175f896
William Budington <bill@eff.org>
2014-01-22 11:57:18 -0800

    Restructuring to add actions into factories.rb.  Adding test for form with captcha.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,9 +2,43 @@ FactoryGirl.define do
   factory :congress_member do
     bioguide_id "A000000"
     success_criteria YAML.dump({"headers"=>{"status"=>200}, "body"=>{"contains"=>"Thank you for your feedback!"}})
+
+    factory :congress_member_with_actions_parent do
+      after(:create) do |c|
+        create :congress_member_action, action: "fill_in", name: 'zip', selector: '#zip', value: "$ADDRESS_ZIP5", required: true, step: 2, congress_member: c    
+        create :congress_member_action, action: "find", selector: '#first-name', step: 3, congress_member: c
+        create :congress_member_action, action: "fill_in", name: 'first-name', selector: '#first-name', value: "$NAME_FIRST", required: true, step: 4, congress_member: c
+        create :congress_member_action, action: "fill_in", name: 'last-name', selector: '#last-name', value: "$NAME_LAST", required: true, step: 5, congress_member: c
+        create :congress_member_action, action: "fill_in", name: 'address', selector: '#address', value: "$ADDRESS_STREET", required: true, step: 6, congress_member: c
+        create :congress_member_action, action: "fill_in", name: 'city', selector: '#city', value: "$ADDRESS_CITY", required: true, step: 7, congress_member: c
+        create :congress_member_action, action: "fill_in", name: 'email', selector: '#email', value: "$EMAIL", required: true, step: 8, congress_member: c
+        create :congress_member_action, action: "check", name: 'email-opt-in', selector: '#email-opt-in', step: 9, congress_member: c
+        create :congress_member_action, action: "uncheck", name: 'email-opt-out', selector: '#email-opt-out', step: 10, congress_member: c
+        create :congress_member_action, action: "choose", name: 'favorite-color', selector: 'input[name=favorite-color][value=green]', step: 11, congress_member: c
+        create :congress_member_action, action: "fill_in", name: 'message', selector: '#message', value: "$MESSAGE", required: true, step: 12, congress_member: c
+        create :congress_member_action, action: "select", name: 'prefix', selector: '#prefix', value: "$NAME_PREFIX", required: true, step: 13, congress_member: c
+      end
+
+      factory :congress_member_with_actions do
+        after(:create) do |c|
+          create :congress_member_action, action: "visit", value: "http://localhost:3001/", step: 1, congress_member: c
+          create :congress_member_action, action: "click_on", selector: 'input[type=submit]', value: "send", step: 14, congress_member: c
+        end
+      end
+
+      factory :congress_member_with_actions_and_captcha do
+        after(:create) do |c|
+          create :congress_member_action, action: "visit", value: "http://localhost:3001/with-captcha", step: 1, congress_member: c
+          create :congress_member_action, action: "fill_in", captcha_selector: "#captcha-image", captcha_id_selector: "#captcha", value: "$CAPTCHA_SOLUTION", step: 14, congress_member: c
+          create :congress_member_action, action: "click_on", selector: 'input[type=submit]', value: "send", step: 15, congress_member: c
+        end
+      end
+
+    end
   end
 
   factory :congress_member_action do
+    congress_member
   end
 
 end
--- spec/models/congress_member_spec.rb
+++ spec/models/congress_member_spec.rb
@@ -32,36 +32,38 @@ describe CongressMember do
       @congress_member.reload
       expect(@congress_member.bioguide_id).to eq("C010101")
     end
+  end
 
-    describe "that has actions" do
-      before do
-        create :congress_member_action, :action => "visit", :value => "http://localhost:3001/", :step => 1, :congress_member => @congress_member
-        create :congress_member_action, :action => "fill_in", :name => 'zip', :selector => '#zip', :value => "$ADDRESS_ZIP5", :required => true, :step => 2, :congress_member => @congress_member    
-        create :congress_member_action, :action => "find", :selector => '#first-name', :step => 3, :congress_member => @congress_member
-        create :congress_member_action, :action => "fill_in", :name => 'first-name', :selector => '#first-name', :value => "$NAME_FIRST", :required => true, :step => 4, :congress_member => @congress_member
-        create :congress_member_action, :action => "fill_in", :name => 'last-name', :selector => '#last-name', :value => "$NAME_LAST", :required => true, :step => 5, :congress_member => @congress_member
-        create :congress_member_action, :action => "fill_in", :name => 'address', :selector => '#address', :value => "$ADDRESS_STREET", :required => true, :step => 6, :congress_member => @congress_member
-        create :congress_member_action, :action => "fill_in", :name => 'city', :selector => '#city', :value => "$ADDRESS_CITY", :required => true, :step => 7, :congress_member => @congress_member
-        create :congress_member_action, :action => "fill_in", :name => 'email', :selector => '#email', :value => "$EMAIL", :required => true, :step => 8, :congress_member => @congress_member
-        create :congress_member_action, :action => "check", :name => 'email-opt-in', :selector => '#email-opt-in', :step => 9, :congress_member => @congress_member
-        create :congress_member_action, :action => "uncheck", :name => 'email-opt-out', :selector => '#email-opt-out', :step => 10, :congress_member => @congress_member
-        create :congress_member_action, :action => "choose", :name => 'favorite-color', :selector => 'input[name=favorite-color][value=green]', :step => 11, :congress_member => @congress_member
-        create :congress_member_action, :action => "fill_in", :name => 'message', :selector => '#message', :value => "$MESSAGE", :required => true, :step => 12, :congress_member => @congress_member
-        create :congress_member_action, :action => "select", :name => 'prefix', :selector => '#prefix', :value => "$NAME_PREFIX", :required => true, :step => 13, :congress_member => @congress_member
-        create :congress_member_action, :action => "click_on", :selector => 'input[type=submit]', :value => "send", :step => 14, :congress_member => @congress_member
-      end
+  v = {
+    "$NAME_FIRST" => "John",
+    "$NAME_LAST" => "Doe",
+    "$ADDRESS_STREET" => "123 Main Street",
+    "$ADDRESS_CITY" => "New York",
+    "$ADDRESS_ZIP5" => "10112",
+    "$EMAIL" => "joe@example.com",
+    "$MESSAGE" => "I have concerns about the proposal....",
+    "$NAME_PREFIX" => "Grand Moff"
+  }
 
-      it "should successfully fill form for a congress member via CongressMember.fill_out_form" do
-        expect(@congress_member.fill_out_form(
-          "$NAME_FIRST" => "John",
-          "$NAME_LAST" => "Doe",
-          "$ADDRESS_STREET" => "123 Main Street",
-          "$ADDRESS_CITY" => "New York",
-          "$ADDRESS_ZIP5" => "10112",
-          "$EMAIL" => "joe@example.com",
-          "$MESSAGE" => "I have concerns about the proposal....",
-          "$NAME_PREFIX" => "Grand Moff"
-        )).to be_true
+  describe "that already exists with actions" do
+    before do
+      @congress_member = create :congress_member_with_actions, :bioguide_id => "B010101"
+    end
+
+    it "should successfully fill form for a congress member via CongressMember.fill_out_form" do
+      expect(@congress_member.fill_out_form(v)).to be_true
+    end
+  end
+
+  describe "that already exists with actions including captcha" do
+    before do
+      @congress_member = create :congress_member_with_actions_and_captcha, :bioguide_id => "B010101"
+    end
+
+
+    it "should successfully fill form for a congress member via CongressMember.fill_out_form" do
+      @congress_member.fill_out_form(v) do |c|
+        "placeholder"
       end
     end
   end
