0c3e6321c88afe81f633232bb0000b521f0bf78b
William Budington <bill@eff.org>
2014-01-23 17:51:15 -0800

    Restructuring controller test to be scoped by route

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -6,26 +6,32 @@ describe "Main controller" do
     expect(last_response.status).to eq(200)
   end
 
-  it "should not raise an exception for nonexistent congress members when requesting form elements from /retrieve-form-elements" do
-    expect do
-      post_json :'retrieve-form-elements', {"bio_ids" => ["B000243", "D000563"]}.to_json
-    end.not_to raise_exception
-    expect(JSON.load(last_response.body)).to eq({})
-  end
+  describe "route /retrieve-from-elements" do
+    before do
+      @route = :'retrieve-form-elements'
+    end
 
-  it "should retrieve form elements successfully from /retrieve-form-elements" do
-    c = create :congress_member_with_actions
-    post_json :'retrieve-form-elements', {"bio_ids" => [c.bioguide_id]}.to_json
-    expect(JSON.load(last_response.body)[c.bioguide_id]).not_to be_nil
-  end
+    it "should not raise an exception for nonexistent congress members when requesting form elements from /retrieve-form-elements" do
+      expect do
+        post_json @route, {"bio_ids" => ["B000243", "D000563"]}.to_json
+      end.not_to raise_exception
+      expect(JSON.load(last_response.body)).to eq({})
+    end
 
-  it "should retrieve form elements successfully with multiple congress members from /retrieve-form-elements" do
-    c = create :congress_member_with_actions
-    c2 = create :congress_member_with_actions, bioguide_id: "A111111"
-    post_json :'retrieve-form-elements', {"bio_ids" => [c.bioguide_id, c2.bioguide_id]}.to_json
+    it "should retrieve form elements successfully from /retrieve-form-elements" do
+      c = create :congress_member_with_actions
+      post_json @route, {"bio_ids" => [c.bioguide_id]}.to_json
+      expect(JSON.load(last_response.body)[c.bioguide_id]).not_to be_nil
+    end
 
-    last_response_json = JSON.load(last_response.body)
-    expect(last_response_json[c.bioguide_id]).not_to be_nil
-    expect(last_response_json[c2.bioguide_id]).not_to be_nil
+    it "should retrieve form elements successfully with multiple congress members from /retrieve-form-elements" do
+      c = create :congress_member_with_actions
+      c2 = create :congress_member_with_actions, bioguide_id: "A111111"
+      post_json @route, {"bio_ids" => [c.bioguide_id, c2.bioguide_id]}.to_json
+
+      last_response_json = JSON.load(last_response.body)
+      expect(last_response_json[c.bioguide_id]).not_to be_nil
+      expect(last_response_json[c2.bioguide_id]).not_to be_nil
+    end
   end
 end
