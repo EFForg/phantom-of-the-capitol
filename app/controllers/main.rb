86529aaeab4ba09208c4834d1ce44f17af5a43ba
William Budington <bill@eff.org>
2014-01-29 11:31:56 -0800

    Adding error checking for json data to main controller

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -4,22 +4,26 @@ CongressForms::App.controller do
   end
 
   post :'retrieve-form-elements' do
+    content_type :json
+    return {status: "error", message: "You must provide bio_ids to retrieve form elements."}.to_json unless params.include? "bio_ids" and params["bio_ids"].is_a? Array
+
     bio_ids = params["bio_ids"]
     response = {}
     bio_ids.each do |bio_id|
       c = CongressMember.bioguide(bio_id)
       response[bio_id] = c.as_required_json unless c.nil?
     end
-    content_type :json
     response.to_json
   end
 
   fh = FillHash.new
   post :'fill-out-form' do
+    content_type :json
+    return {status: "error", message: "You must provide a bio_id, fields, and a uid to fill out form."}.to_json unless params.include? "bio_id" and params.include? "fields" and params.include? "uid"
+
     bio_id = params["bio_id"]
     fields = params["fields"]
     uid = params["uid"]
-    content_type :json
 
     c = CongressMember.bioguide(bio_id)
     return {status: "error", message: "Congress member with provided bio id not found"}.to_json if c.nil?
@@ -31,10 +35,11 @@ CongressForms::App.controller do
   end
 
   post :'fill-out-captcha' do
+    content_type :json
+    return {status: "error", message: "You must provide a uid and answer to fill out captcha."}.to_json unless params.include? "uid" and params.include? "answer"
+
     uid = params["uid"]
     answer = params["answer"]
-
-    content_type :json
 
     return {status: "error", message: "The unique id provided was not found."}.to_json unless fh.include? uid
 
