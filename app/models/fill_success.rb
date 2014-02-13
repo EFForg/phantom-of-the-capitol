fdf95052c2994d81dd42fa4d39b14de3a59b969a
William Budington <bill@eff.org>
2014-02-12 16:44:12 -0800

    Adding initialize override for fill_success model to make campaign_tag transparently

--- app/models/fill_success.rb
+++ app/models/fill_success.rb
@@ -1,4 +1,10 @@
 class FillSuccess < ActiveRecord::Base
   belongs_to :congress_member
   belongs_to :campaign_tag
+
+  def initialize attrs = {}
+    ct = CampaignTag.find_or_create_by_name attrs.delete(:name) if attrs.include? :name
+    super attrs
+    self.campaign_tag = ct
+  end
 end
