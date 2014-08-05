4695b44f6a2bc10e662fb113afe17b77e414e46b
Tony Nguyen <tony.nguyen.81@gmail.com>
2014-08-05 14:03:38 -0400

    Cleaned up script

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # $1 = user to run scripts as
-# $2 = (optional) repository for congress YAML files
 
 # stop setup script if any command fails
 set -e
@@ -11,31 +10,9 @@ then
     DEPENDENCIES="curl imagemagick libmysql++-dev libpq-dev git libqt4-dev xvfb"
 else
     sleep 30
-    DEPENDENCIES="mysql-server"
 fi
 
-# random() {
-#     head -c $1 /dev/urandom | base64
-# }
-
-# mysql_root=$(random 20)
-mysql_congress_forms=""
-# sudo debconf-set-selections <<EOF
-# mysql-server-5.5 mysql-server/root_password password $mysql_root
-# mysql-server-5.5 mysql-server/root_password_again password $mysql_root
-# EOF
-
 su -c "sudo apt-get update; sudo apt-get -y install $DEPENDENCIES" "$1"
-
-# mysql -u root -p"$mysql_root" -e "create database if not exists congress_forms_development;  GRANT ALL PRIVILEGES ON congress_forms_development.* TO 'congress_forms'@'localhost' IDENTIFIED BY '$mysql_congress_forms';"
-# mysql -u root -p"$mysql_root" -e "create database if not exists congress_forms_test;  GRANT ALL PRIVILEGES ON congress_forms_test.* TO 'congress_forms'@'localhost';"
-
-
-# cd /vagrant
-# cp -a config/database-example.rb config/database.rb
-# cp -a config/congress-forms_config.rb.example config/congress-forms_config.rb
-
-# sed -i "s@^  :password.*@  :password => '$mysql_congress_forms',@" config/database.rb
 
 # Doing this to make sure vagrant doesn't install RVM and Ruby as root; there's probably a cleaner way
 if [ "ubuntu" != $1 ]
@@ -47,9 +24,6 @@ su -c "source /home/$1/.rvm/scripts/rvm; rvm use ruby-2.1.0;
 gem install bundler -v 1.5.1;
 rvm gemset create congress-forms; rvm alias create congress-forms ruby-2.1.0@congress-forms; 
 bundle install --path /home/$1/.rvm/gems/ruby-2.1.0@congress-forms/gems/" "$1"
-
-# echo \"Loading schema...\"; bundle exec rake ar:create ar:schema:load > /dev/null;
-# echo \"Loading congress members...\"; bundle exec rake congress-forms:clone_git[$2] > /dev/null;
 
 echo "Setting up PhantomJS..."
 cd /home/$1/
