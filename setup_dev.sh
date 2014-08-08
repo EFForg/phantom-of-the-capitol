8875d96cbbd309219c670a220fe5e80d7df3931c
Tony Nguyen <tony.nguyen.81@gmail.com>
2014-08-08 14:47:02 -0400

    Debugging bash

--- setup_dev.sh
+++ setup_dev.sh
@@ -14,48 +14,38 @@ else
     DEPENDENCIES="mysql-server"
 fi
 
-random() {
-    head -c $1 /dev/urandom | base64
-}
-
-mysql_root=$(random 20)
-mysql_congress_forms=$(random 20)
-sudo debconf-set-selections <<EOF
-mysql-server-5.5 mysql-server/root_password password $mysql_root
-mysql-server-5.5 mysql-server/root_password_again password $mysql_root
-EOF
-
-su -c "sudo apt-get update; sudo apt-get -y install $DEPENDENCIES" "$1"
-
-mysql -u root -p"$mysql_root" -e "create database if not exists congress_forms_development;  GRANT ALL PRIVILEGES ON congress_forms_development.* TO 'congress_forms'@'localhost' IDENTIFIED BY '$mysql_congress_forms';"
-mysql -u root -p"$mysql_root" -e "create database if not exists congress_forms_test;  GRANT ALL PRIVILEGES ON congress_forms_test.* TO 'congress_forms'@'localhost';"
-
-
-cd /vagrant
-cp -a config/database-example.rb config/database.rb
-cp -a config/congress-forms_config.rb.example config/congress-forms_config.rb
-
-sed -i "s@^  :password.*@  :password => '$mysql_congress_forms',@" config/database.rb
-
-# Doing this to make sure vagrant doesn't install RVM and Ruby as root; there's probably a cleaner way
-if [ "ubuntu" != $1 ]
+if [ ! -z $2 ]
 then
-    su -c "curl -sSL https://get.rvm.io | bash -s stable; source /home/$1/.rvm/scripts/rvm; rvm install ruby-2.1.0" "$1"
+	host="export CF_DB_HOST=$2"
+	pwd
+	ls
+	su -c "echo $host >> ~/.bash_profile" "$1"
+	su -c "echo ""export CF_DB_PORT=3306"" >> ~/.bash_profile" "$1"
 fi
 
-su -c "source /home/$1/.rvm/scripts/rvm; rvm use ruby-2.1.0;
-gem install bundler -v 1.5.1;
-rvm gemset create congress-forms; rvm alias create congress-forms ruby-2.1.0@congress-forms; 
-bundle install --path /home/$1/.rvm/gems/ruby-2.1.0@congress-forms/gems/; 
-echo \"Loading schema...\"; bundle exec rake ar:create ar:schema:load > /dev/null;
-echo \"Loading congress members...\"; bundle exec rake congress-forms:clone_git[$2] > /dev/null;" "$1"
+# su -c "sudo apt-get update; sudo apt-get -y install $DEPENDENCIES" "$1"
 
-echo "Setting up PhantomJS..."
-cd /home/$1/
-curl -Lo phantomjs.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-tar -jxvf phantomjs.tar.bz2 > /dev/null
+# # Doing this to make sure vagrant doesn't install RVM and Ruby as root; there's probably a cleaner way
+# if [ "ubuntu" != $1 ]
+# then
+#     su -c "curl -sSL https://get.rvm.io | bash -s stable; source /home/$1/.rvm/scripts/rvm; rvm install ruby-2.1.0" "$1"
+# fi
 
-sudo ln -s /home/$1/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
-sudo chmod go-w /vagrant
+
+
+# source ~/.bash_profile
+
+# su -c "source /home/$1/.rvm/scripts/rvm; rvm use ruby-2.1.0;
+# gem install bundler -v 1.5.1;
+# rvm gemset create congress-forms; rvm alias create congress-forms ruby-2.1.0@congress-forms; 
+# bundle install --path /home/$1/.rvm/gems/ruby-2.1.0@congress-forms/gems/" "$1"
+
+# echo "Setting up PhantomJS..."
+# cd /home/$1/
+# curl -Lo phantomjs.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+# tar -jxvf phantomjs.tar.bz2 > /dev/null
+
+# sudo ln -s /home/$1/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+# sudo chmod go-w /vagrant
 
 echo -e "\n\nYou're all done!  Now type 'vagrant ssh', cd into /vagrant, and type 'bundle exec rackup' to run!"
