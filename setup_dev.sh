574760b0e9bfeb68d8b5ba1792f2bdf1799c0040
Kevin Thayer <kthayer@ngpvan.com>
2014-08-12 09:26:30 -0400

    Update setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -46,6 +46,6 @@ sudo ln -s /home/$1/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomj
 sudo chmod go-w /vagrant
 
 cd
-source ~/.bash_profile
+source .bash_profile
 
 echo -e "\n\nYou're all done!  Now type 'vagrant ssh', cd into /vagrant, and type 'bundle exec rackup' to run!"
