0bbbe71c46870a63851bdbc12a299c7d540c8eb2
Mike Atkins <apeherder@gmail.com>
2014-10-30 11:28:08 -0400

    modify log_files as part of setup

--- deployment/service_setup.sh
+++ deployment/service_setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-sudo -i
+echo "Adding destination to rsyslog_config"
+echo ":destination" >> /vagrant/deployment/log_files.yml
+echo "  port: $RSYSLOG_PORT" >> /vagrant/deployment/log_files.yml
+echo "  host: $RSYSLOG_HOST" >> /vagrant/deployment/log_files.yml
+echo "  protocol: tls" >> /vagrant/deployment/log_files.yml
+
+echo "Upstarting logger"
+sudo cp /vagrant/deployment/upstart-congress-forms-logger.conf /etc/init/upstart-congress-forms-logger.conf
+sudo start upstart-congress-forms-logger
 
 echo "Creating Upstart Script for Congress-Forms"
-cp /vagrant/deployment/upstart-congress-forms.conf /etc/init/upstart-congress-forms.conf
-start upstart-congress-forms
-
-echo env RSYSLOG_PORT=$RSYSLOG_PORT > /etc/init/upstart-congress-forms-logger.conf
-echo env RSYSLOG_HOST=$RSYSLOG_HOST >> /etc/init/upstart-congress-forms-logger.conf
-cat /vagrant/deployment/upstart-congress-forms-logger.conf >> /etc/init/upstart-congress-forms-logger.conf
-start upstart-congress-forms-logger
+sudo cp /vagrant/deployment/upstart-congress-forms.conf /etc/init/upstart-congress-forms.conf
+sudo start upstart-congress-forms
