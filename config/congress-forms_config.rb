1bdb3ff77510487d41ea804a1404ec379fda6141
Mike Atkins <apeherder@gmail.com>
2014-11-07 16:40:25 -0500

    Not SECRET_ACCESS_KEY_ID

--- config/congress-forms_config.rb
+++ config/congress-forms_config.rb
@@ -12,7 +12,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
     aws_access_key_id:     "#{ENV['AWS_ACCESS_KEY_ID']}",
-    aws_secret_access_key: "#{ENV['AWS_SECRET_ACCESS_KEY_ID']}"
+    aws_secret_access_key: "#{ENV['AWS_SECRET_ACCESS_KEY']}"
   }
   config.fog_directory = "#{ENV['FOG_DIRECTORY']}"
 end
