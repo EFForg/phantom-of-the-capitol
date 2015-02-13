e4801e2d5e9c32f38b0c43f7647254a98f4584a3
William Budington <bill@eff.org>
2015-02-13 10:32:55 -0800

    PADRINO_ENV -> RACK_ENV in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
+RACK_ENV = 'test' unless defined?(RACK_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 
