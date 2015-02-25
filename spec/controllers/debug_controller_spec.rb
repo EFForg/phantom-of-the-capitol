223cfce548af8490e2803bc84a14000c331f0c4e
William Budington <bill@eff.org>
2015-02-24 19:04:53 -0800

    Adding tests for debug route /list-actions

--- spec/controllers/debug_controller_spec.rb
+++ spec/controllers/debug_controller_spec.rb
@@ -1,8 +1,39 @@
 require 'spec_helper'
 
 describe "Debug controller" do
-  describe "route /list-congress-members" do
+  describe "route /list-actions" do
+    it "should not be accessable without a correct debug_key" do
+      get '/list-actions/TEST', { debug_key: DEBUG_KEY + "cruft" }
+      expect(last_response.status).to eq(401)
+      last_response_json = JSON.load(last_response.body)
+      expect(last_response_json["status"]).to eq("error")
+    end
 
+    it "should return an error response when a bioguide is not found" do
+      get '/list-actions/Z010101', { debug_key: DEBUG_KEY }
+      last_response_json = JSON.load(last_response.body)
+      expect(last_response_json["status"]).to eq("error")
+    end
+
+    describe "for a member with actions" do
+      before do
+        @c = create :congress_member_with_actions, bioguide_id: "A010101"
+      end
+
+      it "should return a 200 status" do
+        get '/list-actions/' + @c.bioguide_id, { debug_key: DEBUG_KEY }
+        expect(last_response.status).to eq(200)
+      end
+
+      it "should list the correct number of actions" do
+        get '/list-actions/' + @c.bioguide_id, { debug_key: DEBUG_KEY }
+        last_response_json = JSON.load(last_response.body)
+        expect(last_response_json["actions"].count).to eq(@c.actions.count)
+      end
+    end
+  end
+
+  describe "route /list-congress-members" do
     it "should not be accessable without a correct debug_key" do
       get '/list-congress-members', { debug_key: DEBUG_KEY + "cruft" }
       expect(last_response.status).to eq(401)
