87eaefe0cda468ae1a919338802ad6d19be6d6da
William Budington <bill@eff.org>
2015-03-13 12:55:48 -0700

    Only run the schema loading and db create script if the remote db is our docker container

--- docker/phantomdc/entrypoint.sh
+++ docker/phantomdc/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ ! -d "/var/lib/mysql/phantom_dc_$RACK_ENV" ]; then
+if [ ! -d "/var/lib/mysql/phantom_dc_$RACK_ENV" -a `bash -l -c "padrino r \"puts ActiveRecord::Base.configurations[Padrino.env][:host] == ENV['DB_PORT_3306_TCP_ADDR'] && ActiveRecord::Base.configurations[Padrino.env][:password] == ENV['DB_ENV_MYSQL_APP_PASSWORD']\""` == true ]; then
 	echo "Loading schema..."
 	bash -l -c 'bundle exec rake ar:create ar:schema:load' > /dev/null
 	if [ "$RACK_ENV" != "test" ]; then
