fa89b34833370f1f658b33b7b2b9b1ac4ba241bc
Peter Woo <peterw@eff.org>
2017-03-16 11:45:58 -0700

    Start refactoring PerformFills, add more tests

--- app/tasks/perform_fills.rb
+++ app/tasks/perform_fills.rb
@@ -10,66 +10,43 @@ class PerformFills
   end
 
   def execute(args: {})
-    recaptcha_jobs = []
-    captcha_jobs = []
-    noncaptcha_jobs = []
-
-    jobs.each do |job|
-      cm_id, cm_args = DelayedJobHelper::congress_member_id_and_args_from_handler(job.handler)
-      cm = CongressMember::retrieve_cached(cm_hash, cm_id)
-
-      if regex.nil? or regex.match(cm.bioguide_id)
-        if retrieve_captchad_cached(recaptcha_hash, cm.id)
-          recaptcha_jobs.push job
-        elsif retrieve_captchad_cached(captcha_hash, cm.id)
-          captcha_jobs.push job
-        else
-          noncaptcha_jobs.push job
-        end
-      end
-    end
+    recaptcha_jobs, captcha_jobs, noncaptcha_jobs = filter_jobs
 
     if args[:recaptcha_mode].present?
       recaptcha_jobs.each do |job|
-        begin
-          cm_id, cm_args = DelayedJobHelper::congress_member_id_and_args_from_handler(job.handler)
-          cm = CongressMember::retrieve_cached(cm_hash, cm_id)
-          puts red("Job #" + job.id.to_s + ", bioguide " + cm.bioguide_id)
-          pp cm_args
-          result = cm.fill_out_form_with_watir cm_args[0].merge(overrides)
-        rescue
-        end
+        run_job(job, recaptcha: true)
         DelayedJobHelper::destroy_job_and_dependents job
       end
       return
     end
 
     captcha_jobs.each do |job|
-      begin
-        cm_id, cm_args = DelayedJobHelper::congress_member_id_and_args_from_handler(job.handler)
-        cm = CongressMember::retrieve_cached(cm_hash, cm_id)
-        puts red("Job #" + job.id.to_s + ", bioguide " + cm.bioguide_id)
-        pp cm_args
-        result = cm.fill_out_form cm_args[0].merge(overrides), cm_args[1] do |img|
-          puts img
-          STDIN.gets.strip
-        end
-      rescue
+      run_job(job) do |img|
+        puts img
+        STDIN.gets.strip
       end
       DelayedJobHelper::destroy_job_and_dependents job
     end
 
     noncaptcha_jobs.each do |job|
-      begin
-        cm_id, cm_args = DelayedJobHelper::congress_member_id_and_args_from_handler(job.handler)
-        cm = CongressMember::retrieve_cached(cm_hash, cm_id)
-        puts red("Job #" + job.id.to_s + ", bioguide " + cm.bioguide_id)
-        pp cm_args
-        result = cm.fill_out_form cm_args[0].merge(overrides), cm_args[1]
-      rescue
-      end
+      run_job(job)
       DelayedJobHelper::destroy_job_and_dependents job
     end
+  end
+
+  def run_job(job, recaptcha: false, &block)
+    cm_id, cm_args = DelayedJobHelper::congress_member_id_and_args_from_handler(job.handler)
+    cm = CongressMember::retrieve_cached(cm_hash, cm_id)
+
+    puts red("Job #" + job.id.to_s + ", bioguide " + cm.bioguide_id)
+    pp cm_args
+
+    if recaptcha
+      cm.fill_out_form_with_watir cm_args[0].merge(overrides), &block
+    else
+      cm.fill_out_form cm_args[0].merge(overrides), cm_args[1], &block
+    end
+  rescue
   end
 
   def cm_hash
@@ -99,5 +76,28 @@ class PerformFills
   def retrieve_captchad_cached captcha_hash, cm_id
     return captcha_hash[cm_id] if captcha_hash.include? cm_id
     return false
+  end
+
+  def filter_jobs
+    recaptcha_jobs = []
+    captcha_jobs = []
+    noncaptcha_jobs = []
+
+    jobs.each do |job|
+      cm_id, _ = DelayedJobHelper::congress_member_id_and_args_from_handler(job.handler)
+      cm = CongressMember::retrieve_cached(cm_hash, cm_id)
+
+      if regex.nil? or regex.match(cm.bioguide_id)
+        if retrieve_captchad_cached(recaptcha_hash, cm.id)
+          recaptcha_jobs.push job
+        elsif retrieve_captchad_cached(captcha_hash, cm.id)
+          captcha_jobs.push job
+        else
+          noncaptcha_jobs.push job
+        end
+      end
+    end
+
+    [recaptcha_jobs, captcha_jobs, noncaptcha_jobs]
   end
 end
--- spec/tasks/perform_fills_spec.rb
+++ spec/tasks/perform_fills_spec.rb
@@ -8,41 +8,79 @@ describe PerformFills do
   let(:job){ congress_member.delay(queue: "error_or_failure").fill_out_form fields, campaign_tag }
 
   describe ".execute" do
-    it "should call #fill_out_form on each job's congress member" do
+    it "should call #run_job for each job's congress member, and destroy the job afterwards" do
       allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
-      expect(congress_member).to receive(:fill_out_form).with(fields.merge(overrides), campaign_tag)
 
-      PerformFills.new([job], overrides: overrides).execute
-    end
-
-    it "should destroy each job when it's done" do
-      allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
-      expect(congress_member).to receive(:fill_out_form).with(fields.merge(overrides), campaign_tag)
+      task = PerformFills.new([job])
+      expect(task).to receive(:run_job).with(job)
 
       fsj = double
       expect(FillStatusesJob).to receive(:find_by){ fsj }
       expect(fsj).to receive(:destroy)
       expect(job).to receive(:destroy)
 
-      PerformFills.new([job], overrides: overrides).execute
+      task.execute
     end
 
-    context "regex is given" do
-      it "should call #fill_out_form if congress member's bioguide matches the regex" do
+    context "regex was given" do
+      it "should call #run_job if congress member's bioguide matches the regex" do
         allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
-        expect(congress_member).to receive(:fill_out_form).with(fields.merge(overrides), campaign_tag)
 
-        bioguide = congress_member.bioguide_id
-        pattern = /#{bioguide[0, 3]}/
-
-        PerformFills.new([job], regex: pattern, overrides: overrides).execute
+        task = PerformFills.new([job])
+        expect(task).to receive(:run_job).with(job)
+        task.execute
       end
 
-      it "shouldn't call #fill_out_form if congress member's bioguide does not match the regex" do
+      it "shouldn't call #run_job if congress member's bioguide does not match the regex" do
         allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
-        expect(congress_member).not_to receive(:fill_out_form)
 
-        PerformFills.new([job], regex: /^$/, overrides: overrides).execute
+        task = PerformFills.new([job], regex: /^$/)
+        expect(task).not_to receive(:run_job).with(job)
+        task.execute
+      end
+    end
+  end
+
+  describe ".run_job" do
+    it "should call #fill_out_form on the congress member, passing args and respecting overrides" do
+      allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
+
+      expect(congress_member).to receive(:fill_out_form).with(fields.merge(overrides), campaign_tag)
+
+      task = PerformFills.new([job], overrides: overrides)
+      task.run_job(job)
+    end
+
+    it "should swallow exceptions" do
+      allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
+
+      expect(congress_member).to receive(:fill_out_form){ raise ArgumentError.new("oh no") }
+
+      task = PerformFills.new([job], overrides: overrides)
+      expect{ task.run_job(job) }.not_to raise_exception
+    end
+
+    context "recaptcha: true" do
+      it "should call #fill_out_form_with_watir instead" do
+        allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
+
+        expect(congress_member).to receive(:fill_out_form_with_watir).with(fields.merge(overrides))
+
+        task = PerformFills.new([job], overrides: overrides)
+        task.run_job(job, recaptcha: true)
+      end
+    end
+
+    context "block is given" do
+      it "should pass block through to CongressMember#fill_out_form" do
+        allow(CongressMember).to receive(:retrieve_cached).with(anything, congress_member.id.to_s){ congress_member }
+
+        block = Proc.new{}
+
+        expect(congress_member).to receive(:fill_out_form).with(fields.merge(overrides), campaign_tag, &block)
+
+        task = PerformFills.new([job], overrides: overrides)
+        task.run_job(job, &block)
       end
     end
   end
