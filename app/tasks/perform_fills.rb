4e4129662678e99350193f20a07a4b3094acc40b
Peter Woo <peterw@eff.org>
2017-03-21 16:28:39 -0700

    Fix 7bc99a5a065

--- app/tasks/perform_fills.rb
+++ app/tasks/perform_fills.rb
@@ -52,9 +52,9 @@ class PerformFills
         e.errors.each{ |error| warn("  * #{error}") }
         raise e
       end
-    elsif recaptcha_member?(cm) && RACK_ENV == "production"
+    elsif recaptcha_member?(cm) && RACK_ENV != "development"
       cm.fill_out_form_with_watir cm_args[0].merge(overrides), &block
-    elsif RACK_ENV == "production"
+    elsif RACK_ENV != "development"
       cm.fill_out_form cm_args[0].merge(overrides), cm_args[1], &block
     end
   rescue
