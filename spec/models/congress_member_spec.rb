a0c477ed21c417e2e577d3aebe19a3be001fc4f8
Peter Woo <peterw@eff.org>
2017-09-25 16:17:27 -0700

    Remove factory girl congress_member

--- spec/models/congress_member_spec.rb
+++ spec/models/congress_member_spec.rb
@@ -2,14 +2,9 @@ require 'spec_helper'
 
 describe CongressMember do
   describe "that already exists with actions" do
-    before do
-      @congress_member = create :congress_member_with_actions, :bioguide_id => "B010101"
-      @of_iterations = 10
-    end
-
     it "should successfully fill form for a congress member via CongressMember.fill_out_form" do
       v = MOCK_VALUES
-      cm = @congress_member
+      cm = CongressMember.new
       r = cm.fill_out_form(v)
       exp = expect(r)
       exp.to be_truthy
