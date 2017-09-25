cbb3ab85e9537adef42ecd8f30b2826a9c3bed39
Peter Woo <peterw@eff.org>
2017-09-25 16:21:37 -0700

    Simplify failing spec

--- spec/models/congress_member_spec.rb
+++ spec/models/congress_member_spec.rb
@@ -3,11 +3,7 @@ require 'spec_helper'
 describe CongressMember do
   describe "that already exists with actions" do
     it "should successfully fill form for a congress member via CongressMember.fill_out_form" do
-      v = MOCK_VALUES
-      cm = CongressMember.new
-      r = cm.fill_out_form(v)
-      exp = expect(r)
-      exp.to be_truthy
+      CongressMember.new.fill_out_form(MOCK_VALUES)
     end
   end
 end
