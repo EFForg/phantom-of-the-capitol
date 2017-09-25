8958ceb51828a2492f71fe048388c321e96f143e
Peter Woo <peterw@eff.org>
2017-09-25 16:26:34 -0700

    Simplify spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,39 +1,5 @@
 RACK_ENV = ENV["RACK_ENV"] = 'test'
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
-Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
-
-RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
-  conf.include FactoryGirl::Syntax::Methods
-
-  conf.before(:each) do
-    DatabaseCleaner.clean_with :truncation
-    allow_any_instance_of(CaptchaUploader).to receive(:'store!')
-    allow_any_instance_of(ScreenshotUploader).to receive(:'store!')
-  end
-end
-
-# You can use this method to custom specify a Rack app
-# you want rack-test to invoke:
-#
-#   app Myapp::App
-#   app Myapp::App.tap { |a| }
-#   app(Myapp::App) do
-#     set :foo, :bar
-#   end
-#
-def app(app = nil, &blk)
-  @app ||= block_given? ? app.instance_eval(&blk) : app
-  @app ||= Padrino.application
-end
-
-def post_json route, json
-  post route, json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
-end
-
-def put_json route, json
-  put route, json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
-end
 
 FactoryGirl.definition_file_paths = [
   File.join(Padrino.root, 'factories'),
