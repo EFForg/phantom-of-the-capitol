4c30df5cf69dc575e6347cc296d143753975d3e5
Peter Woo <peterw@eff.org>
2017-09-25 16:36:18 -0700

    Try something

--- spec/models/congress_member_spec.rb
+++ spec/models/congress_member_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe CongressMember do
   describe "that already exists with actions" do
     it "should successfully fill form for a congress member via CongressMember.fill_out_form" do
-      CongressMember.new.fill_out_form(MOCK_VALUES)
+      cm = CongressMember.new
+      cm.to_s
+      cm.fill_out_form(MOCK_VALUES)
     end
   end
 end
