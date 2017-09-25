f1b119ce9719294e8adfc150010674751d0bae31
Peter Woo <peterw@eff.org>
2017-09-25 16:51:00 -0700

    Delete the whole app except CongressMember

--- app/models/fill_statuses_job.rb
+++
@@ -1,4 +0,0 @@
-class FillStatusesJob < ActiveRecord::Base
-  belongs_to :fill_status
-  belongs_to :delayed_job, class_name: "::Delayed::Job"
-end
--- app/uploaders/screenshot_uploader.rb
+++
@@ -1,6 +0,0 @@
-class ScreenshotUploader < CarrierWave::Uploader::Base
-  storage :fog
-  def store_dir
-    Padrino.env.to_s + '/screenshots'
-  end
-end
